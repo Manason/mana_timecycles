a35ccd26bb76765136f4b8acda7bee40e32da24d
unknown <clanerp@gmail.com>
2024-01-19 23:57:48 -0800

    initial fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Manason'
+description 'API for playing timecycles'
+version '1.0.0'
+
+client_scripts {
+	'client.lua'
+}
+
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
